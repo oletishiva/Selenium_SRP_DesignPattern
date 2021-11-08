8ea4be97d942b6b57c3f87cfeb455ed24237b7f6
oletishiva <oletishiva@users.noreply.github.com>
2021-11-09 06:55:01 +0700

    adding api test logs

--- src/test/java/com/oleti/seleniumdesign/test/srp/SearchAPITest.java
+++ src/test/java/com/oleti/seleniumdesign/test/srp/SearchAPITest.java
@@ -1,5 +1,6 @@
 package com.oleti.seleniumdesign.test.srp;
 
+import com.oleti.utils.logs.Log;
 import com.oleti.utils.testdata.SearchKeyword;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
@@ -17,24 +18,35 @@ public class SearchAPITest {
     }
 
     @Test(dataProvider = "searchKeyword", dataProviderClass = SearchKeyword.class)
-    public void getRepositoires(String Keyword)
+    public void getRepositoires(String keyword)
     {
-        startTest("API Test Case : Search With Keyword "+ Keyword,"Using API Call Search with Repository Name :" + Keyword);
-        String  response= given().log().all().queryParam("q", Keyword).header("Content-Type","application/json")
+        startTest("API Test Case : Search With Keyword "+ keyword,"Using API Call Search with Repository Name :" + keyword);
+        String  response= given().log().all().queryParam("q", keyword).header("Content-Type","application/json")
                 .when().get("search/repositories")
                 .then().statusCode(200)
                 .extract().response().asString();
 
         JsonPath js=new JsonPath(response);
         int totalRepositiories=js.getInt("total_count");
-        System.out.println("totalRepositories"+totalRepositiories);
-
+        Log.info("Total number of repositories with " +keyword +"="+totalRepositiories);
         if(totalRepositiories>1)
         {
-            for(int i=0;i<10;i++)
-                System.out.println(js.getString("items[" + i + "].full_name"));
-           //  dataInfo(js.getString( "Repository name: items[" + i + "].full_name"));
+            for(int i=0;i<10;i++){
+               /* Log.info(js.getString("full_name: "+ "items[" + i + "].full_name"));
+                Log.info("Repo Url: "+ js.getString("items[" + i + "].owner.url"));
+                Log.info(js.getString("forks: "+"items[" + i + "].forks"));
+                Log.info(js.getString("open_issues: "+"items[" + i + "].open_issues"));
+                Log.info(js.getString("watchers: "+"items[" + i + "].watchers"));
 
+                 */
+
+                 System.out.println(js.getString("items[" + i + "].full_name"));
+                //  dataInfo(js.getString( "Repository name: items[" + i + "].full_name"));
+            }
+        }
+        else
+        {
+            Log.error("Search Keyword "+keyword+" is wrong,try with correct keyword for search");
         }
 
 
